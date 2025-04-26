8b955aac42ee890f6ec0aba533403c08b47ec4e3
lusiulis <lusiulis@gmail.com>
2025-04-25 22:09:59 -0600

    Added MPV controller

--- src-tauri/src/commands/set_wallpaper.rs
+++ src-tauri/src/commands/set_wallpaper.rs
@@ -1,11 +1,11 @@
-use std::process::Command;
 use tauri::{path::BaseDirectory, AppHandle, Manager};
 use windows::core::s;
 use windows::Win32::{
     Foundation::{HWND, LPARAM, WPARAM},
-    UI::WindowsAndMessaging::*,
+    UI::WindowsAndMessaging::{EnumWindows, FindWindowA, FindWindowExA, SendMessageA, SetParent},
 };
 
+use crate::mpv_controller::play_video;
 use crate::windows_utils::{enum_window, wait_for_mpv_window};
 
 #[tauri::command]
@@ -19,45 +19,50 @@ pub fn set_video_as_wallpaper(app: AppHandle, video_path: String) -> Result<(), 
         return Err("mpv.exe no fue encontrado en resources/mpv".to_string());
     }
 
-    let _child = Command::new(mpv_path)
-        .args([
-            &video_path,
-            "--loop",
-            "--no-border",
-            "--geometry=100%x100%",
-            "--osc=no",
-            "--no-input-default-bindings",
-            "--quiet",
-            "--no-audio",
-        ])
-        .spawn()
-        .map_err(|e| format!("Error al iniciar MPV: {}", e))?;
+    let video_result: Result<String, String> = play_video(&video_path, mpv_path);
 
-    let mpv_window: HWND = wait_for_mpv_window()
-        .ok_or("No se encontró la ventana de mpv dentro del tiempo límite")?;
+    if video_result.is_err() {
+        return Err("No se pudo reproducir el video".to_string());
+    }
 
-    unsafe {
-        let progman = FindWindowA(s!("Progman"), None).unwrap();
-        SendMessageA(progman, 0x052C, WPARAM(0), LPARAM(0));
+    let unwraped_video_result = video_result.unwrap();
 
-        let mut worker_w = HWND::default();
-        EnumWindows(
-            Some(enum_window),
-            LPARAM(&mut worker_w as *mut HWND as isize),
-        )
-        .unwrap();
+    match unwraped_video_result.as_str() {
+        "started" => {
+            let mpv_window: HWND = wait_for_mpv_window()
+                .ok_or("No se encontró la ventana de mpv dentro del tiempo límite")?;
 
-        if HWND::is_invalid(&worker_w) {
-            worker_w =
-                FindWindowExA(Some(progman), Some(HWND::default()), s!("WorkerW"), None).unwrap();
+            unsafe {
+                let progman = FindWindowA(s!("Progman"), None).unwrap();
+                SendMessageA(progman, 0x052C, WPARAM(0), LPARAM(0));
+
+                let mut worker_w = HWND::default();
+                EnumWindows(
+                    Some(enum_window),
+                    LPARAM(&mut worker_w as *mut HWND as isize),
+                )
+                .unwrap();
+
+                if HWND::is_invalid(&worker_w) {
+                    worker_w =
+                        FindWindowExA(Some(progman), Some(HWND::default()), s!("WorkerW"), None)
+                            .unwrap();
+                }
+
+                if HWND::is_invalid(&worker_w) {
+                    return Err("No se pudo encontrar una ventana WorkerW válida".to_string());
+                }
+
+                SetParent(mpv_window, Some(worker_w))
+                    .map(|_| ())
+                    .map_err(|e| {
+                        format!("No se pudo establecer la ventana de MPV como fondo: {}", e)
+                    })
+            }
         }
-
-        if HWND::is_invalid(&worker_w) {
-            return Err("No se pudo encontrar una ventana WorkerW válida".to_string());
+        "replaced" => Ok(()),
+        _ => {
+            return Err("No se pudo reproducir el video".to_string());
         }
-
-        SetParent(mpv_window, Some(worker_w))
-            .map(|_| ())
-            .map_err(|e| format!("No se pudo establecer la ventana de MPV como fondo: {}", e))
     }
 }
